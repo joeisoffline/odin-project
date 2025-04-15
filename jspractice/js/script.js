5b1493aa63b69c473820540b8dd3ac0f98380c24
JM <josephma.kw@gmail.com>
2025-04-15 08:21:34 -0600

    practise work

--- jspractice/js/script.js
+++ jspractice/js/script.js
@@ -1,14 +1,5 @@
-console.log((4 + 6 + 9) / 77);
-        
-let a = 36;
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
 
-console.log(a);
-
-let b = 7 * a;
-console.log(b);
-
-const max = 57;
-const actual = max - 13;
-const percentage = actual / max;
-
-console.log(percentage);
+console.log(favoriteAnimal('elephant'))
